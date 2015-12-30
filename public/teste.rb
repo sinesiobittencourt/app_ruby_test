ee9c18d3a7ecca51f3abe021fa684186269ab082
Sinesio Bittencourt <contato@sinesio.com.br>
2015-12-30 20:48:41 -0200

    Teste

---
+++ public/teste.rb
@@ -0,0 +1,14 @@
+<body>
+  <% if nomes.empty? %>
+    <div class="popup">
+      Nenhum nome
+    </div>
+  <% else %>
+    <ul class="listagem">
+      <% nomes.each do |nome| %>
+        <li>
+          <%= nome %></li>
+      <% end %>
+    </ul>
+  <% end %>
+</body>
